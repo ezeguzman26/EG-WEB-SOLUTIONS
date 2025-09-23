cf73d2da783cf61ad78ff8b164b144a791e49596
ezeguzman26 <ezegtalleres14@gmail.com>
2025-09-23 20:12:09 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,35 +1,18 @@
 console.log("Landing EG WEB SOLUTIONS cargada correctamente");
 
-// Toggle menú hamburguesa lateral
+// Toggle menú hamburguesa
 const hamburger = document.getElementById('hamburger');
 const nav = document.getElementById('nav');
-const overlay = document.getElementById('overlay');
-
 
 hamburger.addEventListener('click', () => {
-  nav.classList.toggle('open');
-  overlay.classList.toggle('show');
-});
-
-overlay.addEventListener('click', () => {
-  nav.classList.remove('open');
-  overlay.classList.remove('show');
-});
-
-// Cerrar menú al hacer clic en un enlace
-document.querySelectorAll('#nav a').forEach(link => {
-  link.addEventListener('click', () => {
-    nav.classList.remove('open');
-    overlay.classList.remove('show');
-  });
+  nav.classList.toggle('show');
 });
 
 // Hero parallax en desktop
 const hero = document.querySelector('.hero');
 window.addEventListener('scroll', () => {
-  if (window.innerWidth > 768) {
+  if(window.innerWidth > 768){
     let scrollPos = window.scrollY;
     hero.style.backgroundPositionY = `${scrollPos * 0.5}px`;
   }
 });
-
